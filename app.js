139a71d3be402ad689dbd3f8f64aa9213ab3eecd
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-28 16:54:16 -0400

    vanilla 02 select value and buttons

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 //initial count
 let count = 0;
+
+//select value and all buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
