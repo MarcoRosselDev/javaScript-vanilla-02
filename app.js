452bc2a11b0815c2269b11b0e6ab39965b0d804e
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-28 17:16:09 -0400

    vanilla 02 all ok

--- app.js
+++ app.js
@@ -10,10 +10,19 @@ btns.forEach(function (btn) {
         const styles = e.currentTarget.classList;
         if (styles.contains('decrease')) {
             count--;
-        } else if (styles.contains('reset')) {
-            count = 0;
         } else if (styles.contains('increase')) {
             count++;
+        } else {
+            count = 0;
+        }
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        if (count === 0) {
+            value.style.color = 'black';
         }
         value.textContent = count;
     })
